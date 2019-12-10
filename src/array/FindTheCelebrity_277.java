fd856a0d818ed9b11be89b1e8c74e8b2b3da7f04
YGGGY <yvonneguocn@163.com>
2019-12-10 15:41:57 -0500

    12.10

---
+++ src/array/FindTheCelebrity_277.java
@@ -0,0 +1,23 @@
+package array;
+
+public class FindTheCelebrity_277 {
+    public int findCelebrity(int n) {
+        int candidate = 0;
+        for(int i = 1; i<n; i++){
+            if(knows(candidate, i))//如果candidate认识某个人，那这个candidate一定不会是celebrity
+                candidate = i;//被认识的人有可能是celebrity
+        }
+        //检查candidate是否是celebrity
+        for(int i=0; i<n; i++){
+            //candidate不是celebrity的情况：candidate认识别人/有人不认识candidate
+            if(i != candidate && (knows(candidate,i) || !knows(i, candidate)))
+                return -1;
+        }
+        return candidate;
+    }
+
+    private boolean knows(int a, int b) {
+        /* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+    }
+}
