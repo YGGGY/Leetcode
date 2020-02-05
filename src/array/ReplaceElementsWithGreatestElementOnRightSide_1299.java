bf2a60fe019b531c40d4d1e7d41e7fa9336c912d
YGGGY <yvonneguocn@163.com>
2020-02-05 16:59:14 -0500

    2.5

--- src/array/ReplaceElementsWithGreatestElementOnRightSide_1299.java
+++ src/array/ReplaceElementsWithGreatestElementOnRightSide_1299.java
@@ -1,5 +1,16 @@
 package array;
 
 public class ReplaceElementsWithGreatestElementOnRightSide_1299 {
+    public int[] replaceElements(int[] arr) {
+        int max = arr[arr.length-1];
+        arr[arr.length-1] = -1;
+
+        for(int i = arr.length-2; i>= 0; i--){
+            int temp = arr[i];
+            arr[i] = max;
+            max = Math.max(max, temp);
+        }
+        return arr;
+    }
 
 }
