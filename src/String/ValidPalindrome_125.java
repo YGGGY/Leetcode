2bbe9c7681fb93aabadb4949049d1afc026836c2
YGGGY <yvonneguocn@163.com>
2020-02-11 16:25:08 -0500

    2.11

---
+++ src/String/ValidPalindrome_125.java
@@ -0,0 +1,26 @@
+package String;
+
+public class ValidPalindrome_125 {
+    public boolean isPalindrome(String s) {
+        if(s.isEmpty())
+            return true;
+
+        int left = 0, right = s.length() - 1;
+        char cleft, cright;
+        while(left <= right){
+            cleft = s.charAt(left);
+            cright = s.charAt(right);
+            if(!Character.isLetterOrDigit(cleft))//only number or alphabet count
+                left++;
+            else if(!Character.isLetterOrDigit(cright))
+                right--;
+            else if(Character.toLowerCase(cleft) != Character.toLowerCase(cright))
+                return false;
+            else{
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
--- src/array/SearchA2DMatrix_74.java
+++ src/array/SearchA2DMatrix_74.java
@@ -23,5 +23,27 @@ public class SearchA2DMatrix_74 {
     }
 
     //Turn into 1D array and use binary search
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        int row_num = matrix.length;
+        if(row_num == 0) return false;
+        int column_num = matrix[0].length;
+        if(column_num == 0) return false;
 
+        int left = 0;
+        int right = row_num * column_num - 1;
+        int element;
+        while(left <= right){
+            int mid = (left+right) / 2;
+            //第几行用mid/列数，第几列用mid%列数
+            element = matrix[mid/column_num][mid%column_num];
+            if(element == target)
+                return true;
+            else if(element > target)
+                right = mid -1;
+            else
+                left = mid +1;
+        }
+        return false;
+
+    }
 }
