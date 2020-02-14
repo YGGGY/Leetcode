fb67615cf29b7d7ed43c902a58f6227945de9eee
YGGGY <yvonneguocn@163.com>
2020-02-13 19:21:50 -0500

    2.13

--- src/String/ExpressiveWords_809.java
+++ src/String/ExpressiveWords_809.java
@@ -2,30 +2,62 @@ package String;
 import java.util.*;
 
 public class ExpressiveWords_809 {
+    public int expressiveWords(String S, String[] words) {
+        if(S == null || S.length() == 0 || words == null || words.length==0)
+            return 0;
 
-}
-
-class RLE{
-    String key;
-    List<Integer> counts;
-
-    public RLE(String S){
-        StringBuilder sb = new StringBuilder();
-        counts = new ArrayList();
-
-        char[] Schar = S.toCharArray();
-        int len = Schar.length;
-        int prev = -1;
-
-        for(int i = 0; i < len; i++){
-            if(i == len-1 || Schar[i] != Schar[i+1]){
-                //遍历到最后一个数   //或者这个数不等于后一个数->是单独的数/一连串相等的数中的最后一个
-                sb.append(Schar[i]);//把这个数加进去
-                counts.add(i - prev);//这个数出现的次数=最后一个数出现的index - 一连串数的前一个数的index
-                prev = i;//标志这串数结束计数，用于下串数计数
-            }
+        int result = 0;
+        List<Pair> countS = getCount(S);//
+        for(String w: words){//对words里的每个string检查是否可以
+            if(isMatch(countS, getCount(w)))
+                result++;
         }
+        return result;
+    }
 
-        key = sb.toString();
+    private List<Pair> getCount(String s){
+        List<Pair> count = new ArrayList<>();//list of pair
+        for(int i = 0; i < s.length(); i++){
+            int c = 1;
+            //对每个连续相同的计数
+            while(i < s.length() - 1 && s.charAt(i) == s.charAt(i+1)){//如果和后面的相等，就一直数下去
+                c++;
+                i++;
+            }
+            count.add(new Pair(s.charAt(i),c));
+        }
+        return count;
+    }
+
+    private boolean isMatch(List<Pair> countS, List<Pair> countW){
+        if(countS.size() != countW.size())
+            return false;
+
+        for(int i = 0; i < countS.size(); i++){
+            //字符不相等
+            if(countS.get(i).ch != countW.get(i).ch)
+                return false;
+            //字符相等
+            //字符在S中出现次数<3时，在S和W的次数要相等才行
+            if(countS.get(i).count < 3){
+                if(countS.get(i).count != countW.get(i).count)
+                    return false;
+            }
+            //字符在S中出现次数>=3，只要S的次数>= W的即可通过扩展得到，反言之S的次数< W则不行
+            else if(countS.get(i).count < countW.get(i).count)
+                return false;
+        }
+        return true;
     }
 }
+
+class Pair{
+    char ch;
+    int count;
+
+    public Pair(Character ch, int count){
+        this.ch = ch;
+        this.count = count;
+    }
+}
+
