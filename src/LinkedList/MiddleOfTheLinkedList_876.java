c095734792403f582c50644c94f6e88aa26eeb73
YGGGY <yvonneguocn@163.com>
2020-02-20 12:08:33 -0500

    2.20

--- src/LinkedList/MiddleOfTheLinkedList_876.java
+++ src/LinkedList/MiddleOfTheLinkedList_876.java
@@ -4,7 +4,7 @@ public class MiddleOfTheLinkedList_876 {
     public ListNode middleNode(ListNode head) {
         ListNode[] ans = new ListNode[100];
         int len = 0;
-        while(head.next != null){
+        while(head != null){
             ans[len] = head;
             len++;
             head = head.next;
@@ -13,7 +13,20 @@ public class MiddleOfTheLinkedList_876 {
         //奇数：5/2 = 2 是第三个数
         //偶数： 6/2 = 3 是3 4中后面那个数
     }
+
+    //第二种做法 快慢指针 节省空间
+    //fast pointer到结尾的时候，slow pointer正好在中间
+    public ListNode middleNode2(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
 }
+
 
 class ListNode {
     int val;
