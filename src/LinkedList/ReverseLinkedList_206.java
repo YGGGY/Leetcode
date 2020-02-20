023b043c0c90ac47a0b204e2f9b7e85b0d45ca14
YGGGY <yvonneguocn@163.com>
2020-02-20 16:03:50 -0500

    2.20

--- src/LinkedList/ReverseLinkedList_206.java
+++ src/LinkedList/ReverseLinkedList_206.java
@@ -1,12 +1,38 @@
 package LinkedList;
 
 public class ReverseLinkedList_206 {
-    //iterative
-    //https://www.cnblogs.com/byrhuangqiang/p/4311336.html
+    //Iteration
+    //不用新建一个链表
+    public ListNode reverseList(ListNode head) {
+        //boundary case
+        if(head == null || head.next == null)   return head;
+
+        ListNode prev = null;
+        ListNode curr = head;
+        //prev-curr-temp
+        //每次iterate右移
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;//将尾部的作为head
+    }
+    //新建一个链表的做法 参考21.Merge Two Sorted Lists
 
 
+    //Recursion
+    public ListNode reverseList2(ListNode head) {
+        //base case
+        if(head == null || head.next == null)   return head;
 
-
+        ListNode p = reverseList(head.next);
+        head.next.next = head;//将head->head.next变成head<-head.next（箭头反转）
+        head.next = null;//head变成tail
+        //1->  2<-3<-4<-5 变成 1<-2<-3<-4<-5
+        return p;
+    }
 
 }
 
