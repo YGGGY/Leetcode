513beacdb577635ee3d0829a5eb10f260bd134e6
YGGGY <yvonneguocn@163.com>
2020-03-13 16:31:26 -0400

    3.13

--- src/BinarySearchTree/PathSum_112.java
+++ src/BinarySearchTree/PathSum_112.java
@@ -2,6 +2,7 @@ package BinarySearchTree;
 import java.util.*;
 
 public class PathSum_112 {
+    //iteration
     public boolean hasPathSum(TreeNode root, int sum) {
         if(root==null)  return false;
 
@@ -10,28 +11,41 @@ public class PathSum_112 {
         node_stack.add(root);
         sum_stack.add(sum - root.val);
 
-        TreeNode node;
-        int curr_sum;
+        TreeNode node; //current node
+        int curr_sum; //current remainder
         while(!node_stack.isEmpty()){
+            //get and pop current node
             node = node_stack.pollLast();
             curr_sum = sum_stack.pollLast();
-            //leaf and correct sum
+
+            //check if is leaf and correct sum
             if((node.right == null) && (node.left == null) && (curr_sum == 0))
                 return true;
 
-            //have right subtree
+            //have right subtree, push right node
             if(node.right != null){
                 node_stack.add(node.right);
                 sum_stack.add(curr_sum - node.right.val);
             }
 
-            //have left subtree
+            //have left subtree, push left node
             if(node.left != null){
                 node_stack.add(node.left);
                 sum_stack.add(curr_sum - node.left.val);
             }
         }
         return false;
+    }
+
+
+    //recursion
+    public boolean hasPathSum2(TreeNode root, int sum) {
+        if(root == null)    return false;
+
+        sum -= root.val;
+        if((root.left == null) && (root.right == null))
+            return (sum == 0);
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
 }
 
