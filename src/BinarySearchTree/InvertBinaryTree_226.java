fc7e37d21e0136f040aa0fc024535a6d3f0baa89
YGGGY <yvonneguocn@163.com>
2020-06-04 15:52:21 -0400

    6.4

--- src/BinarySearchTree/InvertBinaryTree_226.java
+++ src/BinarySearchTree/InvertBinaryTree_226.java
@@ -14,7 +14,7 @@ public class InvertBinaryTree_226 {
         root.right = left;
         return root;
     }
-    //Since each node in the tree is visited only once, the time complexity is O(n)O(n), where n is the number of nodes in the tree.
+    //Since each node in the tree is visited only once, the time complexity is O(n), where n is the number of nodes in the tree.
     //O(h) function calls will be placed on the stack in the worst case, where h is the height of the tree. The space complexity is O(n).
 
     //-----------------------------
@@ -38,16 +38,12 @@ public class InvertBinaryTree_226 {
         }
         return root;
     }
-
-
-
-
-
-
-
-
     //We need to swap the left and right child of all nodes in the tree
     //So we create a queue to store the nodes whose left and right child have not been swapped yet
     //Initially, only the root is in the queue
     //As long as the queue is not empty, remove the next node from the queue and swap its children and add the children to the queue
+
+    //Since each node in the tree is visited/added to the queue only once, the time complexity is O(n), where n is the number of nodes in the tree.
+    //Space complexity is O(n), since in the worst case, the queue will contain all nodes in one level of the binary tree.
+
 }
