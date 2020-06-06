6163fbe625384b4157fe2abfbc51d62c1770bf4d
YGGGY <yvonneguocn@163.com>
2020-06-06 16:12:01 -0400

    6.6

---
+++ src/Greedy/QueueReconstructionByHeight_406.java
@@ -0,0 +1,32 @@
+package Greedy;
+import java.util.*;
+
+public class QueueReconstructionByHeight_406 {
+    public int[][] reconstructQueue(int[][] people) {
+        //sort int[][] people
+        Arrays.sort(people, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1, int[] o2){
+                if(o1[0] != o2[0])
+                    return o2[0] - o1[0];//height descending
+                else
+                    //if heights are equal, sort k-values in ascending order
+                    return o1[1] - o2[1];
+            }
+        });
+
+        List<int[]> output = new LinkedList<>();
+        for(int[] p:people){//二维数组people的每项是一个array
+            output.add(p[1], p);//在index==k-value处插入
+        }
+
+        int n = people.length;
+        return output.toArray(new int[n][2]);
+    }
+}
+//Sort the tallest guy in ascending↑ order by k-value, and insert into queue at the indexes equal to k-value.
+//Take the next height in descending↓ order.
+// Also sort these guys in ascending order by k-value, and insert into queue at the indexes equal to k-value.
+//为什么可以这样做呢？因为:
+//Smaller persons are invisible for the taller ones. Then insersion of them won't change the correctness of taller ones.
+//For smaller persons, the number of index means there are exactlly such persons taller or equal to them.
