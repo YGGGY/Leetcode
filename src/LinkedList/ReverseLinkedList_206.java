389eb659cd5d7431bd45ea0641dd62e42075321c
YGGGY <yvonneguocn@163.com>
2020-06-06 22:05:42 -0400

    6.6

--- src/LinkedList/ReverseLinkedList_206.java
+++ src/LinkedList/ReverseLinkedList_206.java
@@ -28,6 +28,7 @@ public class ReverseLinkedList_206 {
         if(head == null || head.next == null)   return head;
 
         ListNode p = reverseList2(head.next);//分成head和其他部分，递归其他部分
+                                            //得到的p是剩余部分reverse之后的新head
         head.next.next = head;//将head->head.next（其他部分的head）
                              // 变成head<-head.next（箭头反转）
         head.next = null;//head变成tail
