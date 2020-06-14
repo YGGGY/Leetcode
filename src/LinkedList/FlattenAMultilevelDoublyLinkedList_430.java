b578ed35e71f9f4ac92108619f4c69703175f83d
YGGGY <yvonneguocn@163.com>
2020-06-14 19:30:49 -0400

    6.14

---
+++ src/LinkedList/FlattenAMultilevelDoublyLinkedList_430.java
@@ -0,0 +1,36 @@
+package LinkedList;
+
+public class FlattenAMultilevelDoublyLinkedList_430 {
+    public Node flatten(Node head) {
+        if(head == null) return null;
+
+        Node curr = head;
+        while(curr != null){
+            if(curr.child == null){
+                curr = curr.next;
+            }
+            else{
+                Node temp = curr.child;
+                while(temp.next != null){
+                    temp  = temp.next;
+                }
+                //child的chain遍历完，插入curr和curr.next之间
+                temp.next = curr.next;
+                if(curr.next != null)
+                    curr.next.prev = temp;
+
+                curr.next = curr.child;
+                curr.child.prev = curr;
+                curr.child = null;
+            }
+        }
+        return head;
+    }
+}
+
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
