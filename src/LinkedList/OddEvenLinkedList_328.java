a194d74677b3bfe60a41189e5dbbe35dc1754726
YGGGY <yvonneguocn@163.com>
2020-06-14 15:19:08 -0400

    6.14

--- src/LinkedList/OddEvenLinkedList_328.java
+++ src/LinkedList/OddEvenLinkedList_328.java
@@ -8,6 +8,8 @@ public class OddEvenLinkedList_328 {
         ListNode even = head.next;
         ListNode evenHead = even;
 
+        //奇数个数的node的话，最后一次开始时是 odd==倒数第三个，even==倒数第二个；结束后 odd==倒数第一，even==null
+        //偶数个数的node的话，最后一次开始时是 odd和even是倒数第二对； 结束后 odd和even是最后一对
         while(even != null && even.next != null){
             odd.next = even.next;
             odd = odd.next;
