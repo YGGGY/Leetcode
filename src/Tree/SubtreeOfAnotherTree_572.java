68c0613da958caa737508c96837f93dba020b5ef
YGGGY <yvonneguocn@163.com>
2020-07-13 15:59:07 -0400

    7.13

---
+++ src/Tree/SubtreeOfAnotherTree_572.java
@@ -0,0 +1,32 @@
+package Tree;
+import java.util.*;
+
+public class SubtreeOfAnotherTree_572 {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(s);
+        while(!queue.isEmpty()){
+            TreeNode curr = queue.poll();
+            if(curr.val == t.val)
+                if(check(curr,t))
+                    return true;
+            if(curr.left != null)
+                queue.add(curr.left);
+            if(curr.right != null)
+                queue.add(curr.right);
+        }
+
+        return false;
+    }
+
+    private boolean check(TreeNode s, TreeNode t){
+        if(s == null && t == null)
+            return true;
+        else if(s == null || t == null)
+            return false;
+        else if(s.val != t.val)
+            return false;
+
+        return check(s.left, t.left) && check(s.right, t.right);
+    }
+}
