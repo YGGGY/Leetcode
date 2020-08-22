6729a6baf5fedafb70d92ebcb51c20a4297c3804
YGGGY <yvonneguocn@163.com>
2020-08-22 13:20:48 -0400

    8.22

--- src/array/BestTimeToBuyAndSellStock_121.java
+++ src/array/BestTimeToBuyAndSellStock_121.java
@@ -2,22 +2,6 @@ package array;
 
 public class BestTimeToBuyAndSellStock_121 {
     //-------------------------------------
-    //股票问题通解
-    public int maxProfit(int[] prices) {
-        if(prices == null || prices.length == 0)  return 0;
-
-        int dp_i0 = 0;
-        int dp_i1 = Integer.MIN_VALUE;
-
-        for(int i = 0; i < prices.length; i++){
-            dp_i0 = Math.max(dp_i0, dp_i1 + prices[i]);
-            dp_i1 = Math.max(dp_i1, - prices[i]);//注意这里不是dp_i0 - prices[i]，因为只能买1次，所以之前一定profit == 0
-        }
-        return dp_i0;
-    }
-
-
-    //-------------------------------------
     //这道题单独的解法，本质是找当前最小数和这个数之后有没有更大的profit（差值）
     public int maxProfit2(int[] prices) {
         int min_price = Integer.MAX_VALUE;
@@ -31,4 +15,22 @@ public class BestTimeToBuyAndSellStock_121 {
         return max_profit;
 
     }
+
+    //-------------------------------------
+    //股票问题通解
+    //K = 1, 新状态只和前一天状态油管，不需要k这个参数，用2个变量表示昨天的[0]和[1]就行
+    //需要注意buy的情况下不能加上dp[i][0]
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0)  return 0;
+
+        int dp_i0 = 0;
+        int dp_i1 = Integer.MIN_VALUE;
+
+        for(int i = 0; i < prices.length; i++){
+            dp_i0 = Math.max(dp_i0, dp_i1 + prices[i]);
+            dp_i1 = Math.max(dp_i1, - prices[i]);//注意这里不是dp_i0 - prices[i]，因为只能买1次，所以之前一定profit == 0
+        }
+        return dp_i0;
+    }
+
 }
--- src/array/BestTimeToBuyAndSellStock_II_122.java
+++ src/array/BestTimeToBuyAndSellStock_II_122.java
@@ -9,7 +9,22 @@ public class BestTimeToBuyAndSellStock_II_122 {
         }
         return max_profit;
     }
+    //只要能想明白，一旦到达短暂的最高价就卖（即在降价前抛售），是最获利的就行
+    //不需要等到最高价再卖，只要降价了，降之前就卖，降了就买。只要有降价的情况，每个爬坡加起来会比总体的高度要大
+
+    //----------------------------------
+    //股票问题通解
+    //可以买无数次，所以不需要k这个参数，用2个变量正常写就行
+    public int maxProfit2(int[] prices) {
+        if(prices == null || prices.length == 0)    return 0;
+
+        int dp_i0 = 0;
+        int dp_i1 = Integer.MIN_VALUE;
+        for(int i = 0; i < prices.length; i++){
+            dp_i0 = Math.max(dp_i0, dp_i1 + prices[i]);
+            dp_i1 = Math.max(dp_i1, dp_i0 - prices[i]);
+        }
+        return dp_i0;
+    }
 }
 
-//只要能想明白，一旦到达短暂的最高价就卖（即在降价前抛售），是最获利的就行
-//不需要等到最高价再卖，只要降价了，降之前就卖，降了就买。只要有降价的情况，每个爬坡加起来会比总体的高度要大
