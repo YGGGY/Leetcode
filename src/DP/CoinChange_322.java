ae112fbf5a18e23da1c3fa294475af0816f3b6c1
YGGGY <yvonneguocn@163.com>
2020-08-25 15:31:36 -0400

    8.25

--- src/DP/CoinChange_322.java
+++ src/DP/CoinChange_322.java
@@ -40,26 +40,24 @@ public class CoinChange_322 {
 
     //Bottom-up iteration
     public int coinChange2(int[] coins, int amount) {
-        if(amount < 1) return 0;
+        if(amount < 1 || coins == null)  return 0;
 
-        int[] dp = new int[amount+1];
-        int sum = 1;//从1开始一点点Bottom-up，计算到sum需要的coins数
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
 
-        while(sum <= amount){//sum为当前计算的需要得到的sum，dp[sum]为得到sum所需要的coins数
-            int min = -1;
-            for(int coin:coins){
-                if(sum >= coin && dp[sum-coin] != -1){//dp[sum-coin]== -1说明达不到这个sum，所以没法在这个sum的基础上加个coin得到新的sum
-                    int temp = dp[sum-coin] + 1;//取之前得出的最低结果
-                    if(min < 0 || temp < min){//min<0说明之前没得出过结果
-                                              //temp<min说明新的结果更好，即min{now,temp}这步
-                        min = temp;
-                    }
-                }
+        for(int i = 1; i < amount + 1; i++){
+            dp[i] = amount + 1; //Initialize 用来检查是否有解 不能用MAX_VALUE，因为后面有+1，会越界
+            for(int coin : coins){
+                if(i - coin < 0)    continue;
+
+                dp[i] = Math.min(dp[i], dp[i-coin] + 1);
             }
-            dp[sum] = min;//min是得到sum这么多amount所需要的coins数的最小值
-            sum++;
         }
-        return dp[amount];
+
+        if(dp[amount] < amount + 1)
+            return dp[amount];
+        else
+            return -1;
     }
 }
 
