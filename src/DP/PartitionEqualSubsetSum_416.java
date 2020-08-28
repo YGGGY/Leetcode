e78beae994aa8887b686ae0a2b37f716cfb0f1ed
YGGGY <yvonneguocn@163.com>
2020-08-27 20:52:31 -0400

    8.27

--- src/DP/PartitionEqualSubsetSum_416.java
+++ src/DP/PartitionEqualSubsetSum_416.java
@@ -21,8 +21,8 @@ public class PartitionEqualSubsetSum_416 {
             dp[i][0] = true;
         }
 
-        for(int i = 1; i <= n; i++){ //背包容量
-            for(int j = 1; j <= sum; j++){ //第i个物品
+        for(int i = 1; i <= n; i++){ //第i个物品
+            for(int j = 1; j <= sum; j++){ //背包容量
                 if(j - nums[i-1] < 0)   //背包放不下第i个物品
                     dp[i][j] = dp[i-1][j]; //不放第i个物品，只能能装满就装满，不能就不能
                 else
@@ -36,5 +36,29 @@ public class PartitionEqualSubsetSum_416 {
     //Space: O(m * n)
 
     //----------------------------------
+    //dp[i][j]的状态只和dp[i-1][j], dp[i-1][j-nums[i]]有关，可以去掉i维度
+    //因为更新j的时候需要看dp[i-1][k](k<j)时的数据，所以j要从后往前遍历。从前往后遍历的话，更新j时会基于新的dp[k]，这个dp[j]实际上是dp[i][k]
+    public boolean canPartition2(int[] nums) {
+        int sum = 0;
+        for(int num : nums){
+            sum += num;
+        }
+        if(sum % 2 != 0)    return false; //总和为奇数，不可能分成2个相等的sum
 
+        int n = nums.length;
+        sum = sum / 2;
+
+        boolean[] dp = new boolean[sum+1];
+        //初始化
+        dp[0] = true;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = sum; j > 0; j--){
+                if(j - nums[i-1] >= 0)   //背包放的下第i个物品
+                    dp[j] = dp[j] || dp[j-nums[i-1]];
+                //不放入第i个物品 vs 放入第i个物品：容量为（总容量-第i个物品）时能装满的话，那总容量就能用第i个物品装满
+            }
+        }
+        return dp[sum];
+    }
 }
