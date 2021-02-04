f16c5de3aee84a6bbb27832c0c990da5392c6609
YGGGY <yvonneguocn@163.com>
2021-02-04 14:35:35 -0500

    2.4

--- src/LinkedList/ReverseNodesInKGroup_025.java
+++ src/LinkedList/ReverseNodesInKGroup_025.java
@@ -5,7 +5,7 @@ public class ReverseNodesInKGroup_025 {
         //find k nodes
         ListNode pointer = head;
         int count = 0;
-        while(count < k) {
+        while(count < k) {//往后找k个node，每次对k个node进行reverse
             if (pointer == null)
                 return head;//less then k nodes left, return head of sub
             else {
@@ -14,8 +14,9 @@ public class ReverseNodesInKGroup_025 {
             }
         }
 
-        ListNode prev = reverseKGroup(pointer, k);
-        //prev = head of sub-problem
+        ListNode prev = reverseKGroup(pointer, k);//prev = head of sub-problem
+                                                // （后面找的k个node reverse之后的新head）
+        //对目前k个node进行reverse
         while(count > 0){
             ListNode temp = head.next;
             head.next = prev;
@@ -23,7 +24,7 @@ public class ReverseNodesInKGroup_025 {
             head = temp;
             count--;
         }
-        return prev;//prev是此时sub的head
+        return prev;//prev是此时reverse后的head
     }
 }
 //所有几个几个一组reverse的做法的通解
