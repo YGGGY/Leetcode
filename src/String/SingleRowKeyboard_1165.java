097aefe6363864f58ba9627c6667b6aea8a60cb6
YGGGY <yvonneguocn@163.com>
2021-03-04 17:12:21 -0500

    3.4

--- src/String/SingleRowKeyboard_1165.java
+++ src/String/SingleRowKeyboard_1165.java
@@ -1,6 +1,6 @@
-package array;
+package String;
 
-public class SingleRowKeyBoard_1165 {
+public class SingleRowKeyboard_1165 {
     public int calculateTime(String keyboard, String word) {
         int[] key = new int[26];
         for(int i = 0; i < 26; i++){
