b66ceab6be77980644c8cf71f0c5a8de5025c434
YGGGY <yvonneguocn@163.com>
2021-03-07 16:07:42 -0500

    3.7

---
+++ src/array/DesignHashMap_706.java
@@ -0,0 +1,27 @@
+package array;
+import java.util.*;
+
+public class DesignHashMap_706 {
+    private int[] arr;
+
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        this.arr = new int[1000001];
+        Arrays.fill(this.arr, -1);
+    }
+
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        this.arr[key] = value;
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        return this.arr[key];
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        this.arr[key] = -1;
+    }
+}
