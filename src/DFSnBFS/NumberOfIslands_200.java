1dd99bdfbfbd3083ebbdbfee7743b0b70829dcad
YGGGY <yvonneguocn@163.com>
2021-03-23 16:30:07 -0400

    3.22

--- src/DFSnBFS/NumberOfIslands_200.java
+++ src/DFSnBFS/NumberOfIslands_200.java
@@ -1,6 +1,8 @@
 package DFSnBFS;
+import java.util.*;
 
 public class NumberOfIslands_200 {
+    //-----------------------dfs--------------------------
     public int numIslands(char[][] grid) {
         int row = grid.length;
         if(row == 0)    return 0;
@@ -28,9 +30,59 @@ public class NumberOfIslands_200 {
         dfs(grid, i, j-1, row, col);
     }
 
-    //Time: O(N) where N = row * col
-    //Space: worst case O(N)
-
+    //Time: O(mn)
+    //Space: worst case O(mn)
     //遍历grid[][]，遇到1之后进行DFS，把这个过程中遇到的1全部变成0
     //每次触发一次DFS，次数+1，最后的总次数就是小岛数量
+
+
+
+    //-----------------------bfs------------------------
+    //把1的点改为0压入queue，然后每次从queue取一个，找上下左右为1的点改为0压入queue
+    public int numIslands2(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1'){
+                    count ++;
+                    grid[i][j] = '0';
+                    Queue<int[]> queue = new LinkedList<>();
+                    queue.add(new int[]{i,j});
+                    while(!queue.isEmpty()){
+                        int[] curr = queue.poll();
+                        int row = curr[0];
+                        int col = curr[1];
+                        if(row + 1 < n && grid[row+1][col] == '1'){
+                            grid[row+1][col] = '0';
+                            queue.add(new int[]{row+1, col});
+                        }
+                        if(row - 1 >= 0 && grid[row-1][col] == '1'){
+                            grid[row-1][col] = '0';
+                            queue.add(new int[]{row-1, col});
+                        }
+                        if(col + 1 < m && grid[row][col+1] == '1'){
+                            grid[row][col+1] = '0';
+                            queue.add(new int[]{row, col+1});
+                        }
+                        if(col - 1 >= 0 && grid[row][col-1] == '1'){
+                            grid[row][col-1] = '0';
+                            queue.add(new int[]{row, col-1});
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    //Time: O(mn)
+    //Space: O(min(m, n))
+
+
+
+    //-------------------union find-----------------------
+
+
 }
---
+++ src/array/ThreeSum_015.java
@@ -0,0 +1,67 @@
+package array;
+import java.util.*;
+
+public class ThreeSum_015 {
+    //--------------------------------------------------
+    //对一个选定的数nums[i]，再找两个数，使两数之和为-nums[i]
+    //用two pointer做，和Two Sum II做法一样----注意能这么做是因为是sorted后的
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> output = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0) break;
+            if(i == 0 || nums[i] != nums[i-1]){//去掉相同的数
+                int left = i + 1, right = nums.length-1;
+                while(left < right){
+                    int sum = nums[i] + nums[left] + nums[right];
+                    if(sum == 0){
+                        List<Integer> temp = Arrays.asList(nums[i], nums[left], nums[right]);
+                        output.add(temp);
+                        left++;
+                        right--;
+                        while(nums[left] == nums[left-1] && left < right){ //去掉相同的数
+                            left ++;
+                        }
+                    }
+                    else if(sum < 0)
+                        left ++;
+                    else
+                        right --;
+                }
+            }
+        }
+        return output;
+    }
+    //Time: O(n^2)
+
+    //-----------------------
+    //另一种做法，也是要sort，然后i从0开始取。然后剩下两个数和two sum一样，用one pass+hashset来找
+    //Time也是O(n^2)
+
+
+
+    //------------------------------------------------
+    //不用sort的方法
+    public List<List<Integer>> threeSum2(int[] nums) {
+        HashSet<List<Integer>> output = new HashSet<>(); //用set来去掉重复的结果
+        HashSet<Integer> duplicate = new HashSet<>();  //去除nums里重复的数
+        HashMap<Integer, Integer> visited = new HashMap<>(); //nums[j]->i，每个i的visited单独来看
+        for(int i = 0; i < nums.length; i++){
+            if(!duplicate.contains(nums[i])){ //重复的数直接跳过
+                duplicate.add(nums[i]);
+                for(int j = i + 1; j < nums.length; j++){
+                    int complement = -nums[i] - nums[j];
+                    //在这个i的轮次找到过complement
+                    if(visited.containsKey(complement) && visited.get(complement) == i){
+                        List<Integer> temp = Arrays.asList(nums[i], nums[j], complement);
+                        Collections.sort(temp);
+                        output.add(temp);
+                    }
+                    visited.put(nums[j], i);
+                }
+            }
+        }
+        return new ArrayList(output);
+    }
+
+}
