b6c17ce5282cb06ed28f46c87ee0a354074b1f96
YGGGY <yvonneguocn@163.com>
2021-03-28 00:29:42 -0400

    3.27

--- src/DFSnBFS/NumberOfIslands_200.java
+++ src/DFSnBFS/NumberOfIslands_200.java
@@ -83,6 +83,73 @@ public class NumberOfIslands_200 {
 
 
     //-------------------union find-----------------------
+    //别的unionfind都是邻接表/邻接矩阵，矩阵是n*n的，一共n个点，0~n-1正好代表每个点
+    //但这题有n*m个点，所以每个点[i,j]用i*col+j来表示
+    public int numIslands3(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        UnionFind uf = new UnionFind(grid);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1'){
+                    for(int[] direction : directions){
+                        int row = i + direction[0];
+                        int col = j + direction[1];
+                        if(row >= 0 && row < n && col >= 0 && col < m && grid[row][col] == '1'){
+                            int id1 = i * m + j;
+                            int id2 = row * m + col;
+                            uf.union(id1, id2);
+                        }
+                    }
+                }
+            }
+        }
+        return uf.getCount();
+    }
+    //Time: O(mn)
+    //Space: O(mn)
+}
 
+class UnionFind{
+    private int[] parents;
+    //private int[] rank;    //这次其实不用管rank，反正联通的一定会并一起就行了
+    private int count; //用count数来记录cluster数，每加一个node时+1，union一次时-1
 
+    public UnionFind(char[][] grid){ //传入matrix，把所有为1的格子初始化为节点，节点id为i * col + j
+        int n = grid.length;
+        int m = grid[0].length;
+        parents = new int[n*m];
+        //rank = new int[n*m];
+        count = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1'){
+                    int id = i * m + j;
+                    parents[id] = id;
+                    count ++;
+                }
+            }
+        }
+    }
+
+    public int find(int x){
+        if(parents[x] != x)
+            parents[x] = find(parents[x]);
+        return parents[x];
+    }
+
+    public void union(int x, int y){
+        int px = find(x);
+        int py = find(y);
+        if(px == py)    return;
+
+        if(px != py)
+            parents[py] = px;
+        count --;
+    }
+
+    public int getCount(){
+        return count;
+    }
 }
