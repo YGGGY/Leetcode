7ee9a650044718b69a4806484fce5278e297d7e6
YGGGY <yvonneguocn@163.com>
2021-04-22 22:49:56 -0400

    4.22

--- src/array/ProductOfArrayExceptSelf_238.java
+++ src/array/ProductOfArrayExceptSelf_238.java
@@ -2,46 +2,52 @@ package array;
 
 public class ProductOfArrayExceptSelf_238 {
     public int[] productExceptSelf(int[] nums) {
-        int length = nums.length;
-        int[] left = new int[length];
-        int[] right = new int[length];
-        int[] result = new int[length];
-        left[0] = nums[0];
-        right[length-1] = nums[length-1];
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] result = new int[n];
 
-        for(int i = 1; i < length; i++){
+        left[0] = nums[0];
+        for(int i = 1; i < n; i++){
             left[i] = left[i-1] * nums[i];
         }
-        for(int i = length-2; i > 0; i--){
+        right[n-1] = nums[n-1];
+        for(int i = n-2; i >= 0; i--){
             right[i] = right[i+1] * nums[i];
         }
-        for(int i = 1; i < length-1; i++){
+
+        result[0] = right[1];
+        result[n-1] = left[n-2];
+        for(int i = 1; i < n-1; i++){
             result[i] = left[i-1] * right[i+1];
         }
-        result[0] = right[1];
-        result[length-1] = left[length-2];
         return result;
     }
 
-//the product of the other and be get by multiply left part and right part
-//use arrays left[] and right[] to record the product of left part and right part
+//算出从左往右乘积left[]，从右往左乘积right[]， result[i] = left[i-1] * right[i+1]
+//Time:O(n)
+//Space: O(n)
 
-///To save space
-    public int[] productExceptSelf_2(int[] nums) {
-        int length = nums.length;
-        int[] result = new int[length];
 
-//same with the former array
-        for(int i = 1; i < length; i++){
-            result[i] = result[i-1] * nums[i];
-        }
-//use int r to save the right part product
-        int r = 1;
-        for(int i = length-1; i >=0; i--){
-            result[i] = result[i] * r;
-            r *= nums[i];
-        }
+//-----------------------------------------------
+//节省空间的做法，从左往右的乘积直接记录在result里，再从右往左时用right变量记录right部分，直接当场计算result[i]
+public int[] productExceptSelf2(int[] nums) {
+    int n = nums.length;
+    int[] result = new int[n];
 
-        return result;
+    result[0] = nums[0];
+    for(int i = 1; i < n-1; i++){//从第一个乘到到倒数第二个
+        result[i] = result[i-1] * nums[i];
     }
+
+    result[n-1] = result[n-2];//最后的数为前n-1个数之积
+    int right = nums[n-1];
+    for(int i = n-2; i >= 1; i--){ //right从倒数第二个数乘到第二个数
+        result[i] = result[i-1] * right;
+        right *= nums[i];
+    }
+    result[0] = right;
+
+    return result;
+}
 }
